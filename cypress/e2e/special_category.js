8fac7c13f3d2dd4b7f3f76fe4f50b79f70c61ce5
k02s <khsaleh022@gmail.com>
2023-08-28 18:28:24 +0300

    update

--- cypress/e2e/special_category.js
+++ cypress/e2e/special_category.js
@@ -4,20 +4,43 @@ describe('Specials category testing', () => {
 
         cy.visit("https://www.automationteststore.com/")
 
+        // initialize variables for sum
+        var SumOldPrices = 0;
+        var SumNewPrices = 0;
+
+        // sum all old prices 
         cy.get('#special > .container-fluid')
             .find(".priceold")
-            .invoke("text")
-            .then((old_price) => {
-                cy.log(old_price)
+            .each(($item) => {
+                cy.wrap($item)
+                    .invoke("text")
+                    .then((old_price) => {
+                        let OldPrice = parseFloat(old_price.replace("$", ""));
+                        SumOldPrices += OldPrice;
+                        cy.log(SumOldPrices)
+                    })
             })
+            
+        // alert msg for sum old prices
+           alert(`the sum for all old prices is:  ${SumOldPrices}`)
 
+        // sum all new prices 
         cy.get('#special > .container-fluid')
             .find(".pricenew")
-            .invoke("text")
-            .then((new_price) => {
-                cy.log(new_price)
+            .each(($item) => {
+                cy.wrap($item)
+                    .invoke("text")
+                    .then((new_price) => {
+                        let NewPrice = parseFloat(new_price.replace("$", ""));
+                        SumNewPrices += NewPrice;
+                        cy.log(SumNewPrices)
+                    })
             })
+        
+        // alert msg for sum new prices
+           alert(`the sum for all new prices is:  ${SumNewPrices}`)
 
-    });
+        
+    });// end test case
 
-});
+});// end suit case
